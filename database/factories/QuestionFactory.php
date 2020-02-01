ebc05a99b425885c26c5d9270d3b4605657bd94a
Rashid <soljer133@gmail.com>
2020-02-01 20:27:06 +0500

    factroy set for Question

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -6,7 +6,9 @@ use App\Model\Question;
 use Faker\Generator as Faker;
 
 $factory->define(Question::class, function (Faker $faker) {
+    $title =  $faker->sentence();
     return [
-        //
+        'title' => $faker->sentence(),
+        'slug' => str_slug($title),
     ];
 });
