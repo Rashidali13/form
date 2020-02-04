86bceb51347f88987ff204cb222cdd9fedccff33
Rashid <soljer133@gmail.com>
2020-02-04 16:16:56 +0500

    migration modified

--- database/migrations/2020_02_01_141646_create_questions_table.php
+++ database/migrations/2020_02_01_141646_create_questions_table.php
@@ -20,6 +20,7 @@ class CreateQuestionsTable extends Migration
             $table->text('body');
             $table->bigInteger('category_id')->unsigned();
             $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
